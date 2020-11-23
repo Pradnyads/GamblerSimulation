4108a98d88531d2c098e867beb04078e8e27af56
Pradnyads <sorepradnya@gmail.com>
2020-11-23 16:58:32 +0530

    Modified - Gambler is win or loss the bet

--- GamblerSimulation.sh
+++ GamblerSimulation.sh
@@ -1,22 +1,33 @@
 #!/bin/bash 
 #UC1
+
 stake=100
-betammount=1
-#UC2
+bet=1
+
+# UC2 Condition to check win or loss
+# UC3 50% of amount won or loss
 win=0
 loss=0
- (( noOfPlay++ ))
- stake=$(( $stake - 1 ))
- play=$(( RANDOM%2 ))
+noOfPlay=0
+while [[ $bet -le 50 && $bet -gt 0 ]]
+do
 
- # Condition to check win or loss
- if [[ $play -eq 1 ]]
- then
-     (( win++ ))
-     stake=$(( $stake + 2 ))
-     echo " Gambler win "
- else
-     (( loss++ ))
-     echo " Gambler loss"
- fi
+        (( noOfPlay++ ))
+	bet=$(($bet + 1 ))
+	play=$(( RANDOM%2 ))
+
+	if [[ $play -eq 1 ]]
+	then
+          (( win++ ))
+          echo " Gambler is win "
+	else
+          (( loss++ ))
+          echo " Gambler is loss"
+	fi
+done
+
+echo "No of play=$noOfPlay "
+echo "No of wins=$win "
+echo "No if loss=$loss"
+
 
