a4258b846f19138b281e16b54e13a6ecbb65e0bb
Pradnyads <72643288+Pradnyads@users.noreply.github.com>
2020-11-23 16:22:37 +0530

    Update GamblerSimulation.sh

--- GamblerSimulation.sh
+++ GamblerSimulation.sh
@@ -2,21 +2,12 @@
 #UC1
 stake=100
 betammount=1
-#UC2
-win=0
-loss=0
- (( noOfPlay++ ))
- stake=$(( $stake - 1 ))
+#UC2 Condition to check win or loss
  play=$(( RANDOM%2 ))
-
- # Condition to check win or loss
  if [[ $play -eq 1 ]]
  then
-     (( win++ ))
-     stake=$(( $stake + 2 ))
-     echo " Gambler win "
+      echo " Gambler win "
  else
-     (( loss++ ))
-     echo " Gambler loss"
+      echo " Gambler loss"
  fi
 
